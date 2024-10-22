4703fc88b17f13163f016ac35943e3d0018f5845
Marko Tunjic <marko.tunjic@platomics.com>
2024-10-22 23:11:47 +0200

    Create initial config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { GamesOverviewComponent } from './features/games-overview/games-overview.component';
+import { importProvidersFrom } from '@angular/core';
+import { NgxsModule } from '@ngxs/store';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: GamesOverviewComponent,
+    providers: [importProvidersFrom(NgxsModule.forFeature([]))],
+  },
+];
---
+++ src/app/features/games-overview/games-overview.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-games-overview',
+  standalone: true,
+  imports: [],
+  templateUrl: './games-overview.component.html',
+})
+export class GamesOverviewComponent {}
